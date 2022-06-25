246c195568c8839a1c8f14f7ad3d4c74a0cfc257
Alindear <1425211870@qq.com>
2022-06-26 00:33:49 +0800

    联系工棚

--- houtai/src/contactus.vue
+++ houtai/src/contactus.vue
@@ -6,28 +6,56 @@
                     :src="icon1"
                     alt=""
                 >
-                <p>DISCORD</p>
+                <p>
+                    DISCORD
+                    <span>
+                        <img
+                            :src="enterPng"
+                            alt=""
+                        >
+                    </span>
+                </p>
             </div>
             <div class="comm_list color2">
                 <img
                     :src="icon2"
                     alt=""
                 >
-                <p>MEDIUM</p>
+                <p>
+                    MEDIUM
+                    <span>
+                        <img
+                            :src="enterPng"
+                            alt=""
+                        >
+                    </span>
+                </p>
             </div>
             <div class="comm_list color3">
                 <img
                     :src="icon3"
                     alt=""
                 >
-                <p>TWITTER</p>
+                <p>
+                    TWITTER
+                    <img
+                        :src="enterPng"
+                        alt=""
+                    >
+                </p>
             </div>
             <div class="comm_list color4">
                 <img
                     :src="icon4"
                     alt=""
                 >
-                <p>TELEGRAM PERSONAL</p>
+                <p>
+                    TELEGRAM PERSONAL
+                    <img
+                        :src="enterPng"
+                        alt=""
+                    >
+                </p>
             </div>
         </div>
     </div>
@@ -39,6 +67,7 @@ import icon1 from 'img/iconus/discord@2x.png';
 import icon2 from 'img/iconus/编组 28@2x.png';
 import icon3 from 'img/iconus/推特@2x.png';
 import icon4 from 'img/iconus/telegram@2x.png';
+import enterPng from 'img/首页/编组 24@2x.png';
 export default {
 	components: { headEr },
 	data() {
@@ -47,6 +76,7 @@ export default {
 			icon2,
 			icon3,
 			icon4,
+			enterPng,
 		};
 	},
 
@@ -94,10 +124,42 @@ export default {
 				font-family: Womby-Regular;
 				font-weight: 400;
 				font-size: 0.34rem;
+				img {
+					display: none; //PC没有箭头
+				}
+			}
+		}
+		@media (max-width: 750px) {
+			.comm_list {
+				width: 6.22rem;
+				height: 2.52rem;
+				margin-right: 0;
+				padding: 0.32rem 0.32rem 0.335rem;
+				margin-bottom: 0.165rem;
+				box-sizing: border-box;
+				img {
+					width: 0.44rem;
+					height: 0.44rem;
+					margin-bottom: 1.02rem;
+				}
+				p {
+					margin: 0;
+					height: 0.41rem;
+					font-family: Womby-Regular;
+					font-weight: 400;
+					font-size: 0.34rem;
+					display: flex;
+					justify-content: space-between;
+					box-sizing: border-box;
+					img {
+						display: block;
+						margin-right: 0.382rem;
+					}
+				}
 			}
 		}
 
-		i.comm_listmg:nth-child(2n) {
+		.comm_list:nth-child(2n) {
 			margin-right: 0;
 		}
 		.color1 {
@@ -123,6 +185,14 @@ export default {
 			border: 0.01rem solid rgba(38, 190, 255, 0.3);
 			border-radius: 0.32rem;
 			color: #5ecdb2;
+		}
+	}
+	@media (max-width: 750px) {
+		.contact_module {
+			width: 6.86rem;
+			height: 11.22rem;
+			margin: 0.32rem auto 0.54rem;
+			padding: 0.32rem;
 		}
 	}
 }
