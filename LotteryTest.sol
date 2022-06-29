1cb63e955fa317e668368c6cec37e30d169cdbb0
cuilele <cuilele@pin-dao.cn>
2022-06-29 18:37:22 +0800

    LotteryTest.sol

---
+++ LotteryTest.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: MIT
+// An example of a consumer contract that relies on a subscription for funding.
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+interface BNSRegistry{
+    function freeBuy(bytes32[] memory nodes, address[] memory owners, address[] memory _holders,uint256 _years) external;
+}
+contract Lottery is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    uint64 s_subscriptionId;
+    // TODO 测试配置
+    address vrfCoordinator = 	0x6A2AAd07396B36Fe02a22b33cf443582f682c82f;
+    bytes32 keyHash = 0xd4bb89654db74673a187bd804519e65e3f71a52bc55f11da7601a13dcf505314;
+    address public BNSAddress = 0x6A2AAd07396B36Fe02a22b33cf443582f682c82f;
+    BNSRegistry public _bns = BNSRegistry(BNSAddress);
+    uint32 callbackGasLimit = 1000000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords =  1;
+    uint256[] public s_randomWords;
+    uint256 public s_requestId;
+    address s_owner;
+
+    struct Cycle {
+        bytes32[] lotteryPool; // 奖池
+        address[] holders; // 中奖名单
+        mapping(address=>bytes32) holderLottery; // 中奖映射
+        mapping(bytes32=>address) lotteryHolder; // 中奖反向映射
+        mapping(address=>bool) palyers; // 本次已参与人员,周期清理
+        uint256 startTime; // 起始shijian
+        uint256 dayNum; // 一次循环的天数
+    }
+
+    uint256 public dayNum; // 全局配置：一次循环的天数
+    Cycle[] public cycles; // 历史记录
+    uint256 index=0; // 当前周期的下标
+    uint256 rangeRate = 10; // 抽奖范围为奖池的10倍
+
+
+    function setDayNum(uint256 _dayNum) public onlyOwner{
+        dayNum = _dayNum;
+        cycles[index].dayNum = _dayNum;
+    }
+    function setStartTime(uint256 _start) public onlyOwner{
+        cycles[index].startTime = _start;
+    }
+
+    function initCycle(uint256 _startTime,bool init,uint256 _dayNum,bytes32[] memory _lotteryPool) internal{
+        cycles.push();
+        if(!init) index++;
+        cycles[index].startTime  = _startTime;
+        cycles[index].dayNum  = _dayNum;
+        cycles[index].lotteryPool  = _lotteryPool;
+    }
+
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        initCycle(block.timestamp,true,7,new bytes32[](0));
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
+    }
+
+    function requestRandomWords() public onlyOwner {
+        s_requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords = randomWords;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == s_owner);
+        _;
+    }
+
+
+    function randomLottery() public{
+        // 0. 校验用户是否已参与
+        require(cycles[index].palyers[msg.sender],"address already lottery");
+        // 1. 校验时间是否已经过期,提前一小时关闭奖池
+        require(checkEnable(),"lottery is ending");
+        // 2. 拿到random seek,加上当前时间进行range*奖池大小取余数
+        uint256 lottertRange =cycles[index].lotteryPool.length + rangeRate;
+        uint256 myIndex= (s_randomWords[0]+block.timestamp)%lottertRange;
+        if(myIndex<cycles[index].lotteryPool.length
+            && cycles[index].lotteryHolder[cycles[index].lotteryPool[myIndex]]==address(0x0) ){
+            cycles[index].lotteryHolder[cycles[index].lotteryPool[myIndex]] = msg.sender;
+            cycles[index].holderLottery[msg.sender] = cycles[index].lotteryPool[myIndex];
+            cycles[index].holders.push(msg.sender);
+        }
+        // 3. 设置其他项
+        cycles[index].palyers[msg.sender] = true;
+    }
+
+    function getMyLottery() public view returns (bytes32) {
+        if(!checkEnable()) return 0;
+        return cycles[index].holderLottery[msg.sender];
+    }
+
+
+    function checkEnable() public view returns (bool) {
+        return block.timestamp < (cycles[index].startTime+cycles[index].dayNum*86400-3600);
+    }
+    // TODO 临时替换奖池的方法
+
+    function openLottery(bytes32[] memory _lotteryPool,uint256 _startTime,uint256 _dayNum) public onlyOwner{
+        initCycle(_startTime,false,_dayNum,_lotteryPool);
+    }
+
+    function closLottery() public onlyOwner{
+        // TODO 1. 关闭cycle
+        // 2. 分发奖池nft
+        if(cycles[index].holders.length==0) return;
+        bytes32[] memory nodes = new bytes32[](cycles[index].holders.length);
+        for(uint i=0;i<cycles[index].holders.length;i++){
+            nodes[i] = cycles[index].holderLottery[cycles[index].holders[i]];
+        }
+
+        _bns.freeBuy(nodes,cycles[index].holders,cycles[index].holders,1);
+    }
+
+    function setBns(address _bnsAddr) public onlyOwner{
+        BNSAddress = _bnsAddr;
+        _bns = BNSRegistry(BNSAddress);
+    }
+
+}
